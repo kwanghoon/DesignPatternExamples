f62c0f40207a4f28d035fba1c101812b7d1cd173
khChoi <lazyswap@gmail.com>
2018-05-30 14:06:49 +0900

    Added a test suite and a test runner.

---
+++ DesignPatternExamples/src/com/example/designpattern/state/test/AllTestsforOnOffLight.java
@@ -0,0 +1,11 @@
+package com.example.designpattern.state.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ LightTest.class, LightTest.class })
+public class AllTestsforOnOffLight {
+
+}
